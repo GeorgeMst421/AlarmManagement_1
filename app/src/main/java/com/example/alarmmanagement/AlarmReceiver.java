c704e0f070183b791a845feb35ad6e6f1f98b4b4
George <moustosgeorge@gmail.com>
2023-04-08 16:45:06 +0300

    RemoteViews @supressLint

--- app/src/main/java/com/example/alarmmanagement/AlarmReceiver.java
+++ app/src/main/java/com/example/alarmmanagement/AlarmReceiver.java
@@ -1,15 +1,17 @@
 package com.example.alarmmanagement;
 
+import android.annotation.SuppressLint;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
-public class    AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
 
@@ -17,18 +19,21 @@ public class    AlarmReceiver extends BroadcastReceiver {
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(context,0,i,PendingIntent.FLAG_IMMUTABLE);
 
+        @SuppressLint("RemoteViewLayout") RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.activity_destination);
+        remoteViews.setTextViewText(R.id.textView2,"BIGUS ");
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context,"georgeid")
                 .setSmallIcon(R.drawable.ic_launcher_background)
                 .setContentTitle("George Alarm Manager")
                 .setContentText("This is a notification")
                 .setAutoCancel(true)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setDefaults(NotificationCompat.DEFAULT_ALL)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent);
+                .setContentIntent(pendingIntent)
+                .setCustomContentView(remoteViews);
 
         NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(context);
         notificationManagerCompat.notify(123,builder.build());
-
-
     }
 }
